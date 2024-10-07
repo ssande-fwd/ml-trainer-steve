2ba27f949e3f739f6f2a4fd08123413260a64b62
Robert Knight <robert@microbit.org>
2024-10-07 17:33:08 +0100

    Bump extension version

--- src/makecode/utils.ts
+++ src/makecode/utils.ts
@@ -18,7 +18,7 @@ export const filenames = {
 
 // Exported for testing.
 export const extensionName = "machine-learning";
-const extensionURL = "github:microbit-foundation/pxt-microbit-ml#v0.4.4";
+const extensionURL = "github:microbit-foundation/pxt-microbit-ml#v0.4.5";
 
 export const pxt = {
   name: "Untitled",
