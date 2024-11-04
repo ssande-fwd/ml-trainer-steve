58965623a0bcb6b30b35bd61d9fec6866405da5e
Robert Knight <95928279+microbit-robert@users.noreply.github.com>
2024-11-04 17:15:34 +0000

    Use brand color for arrow SVGs (#466)

--- src/components/ArrowOne.tsx
+++ src/components/ArrowOne.tsx
@@ -1,6 +1,7 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, useToken } from "@chakra-ui/react";
 
 const ArrowOne = () => {
+  const [brand500] = useToken("colors", ["brand2.500"]);
   return (
     <svg
       width="250"
@@ -9,9 +10,9 @@ const ArrowOne = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect x="35" y="15" width="180" height="10" fill="#00a000" />
-      <circle cx="230" cy="20" r="20" fill="#00a000" />
-      <path d="M0 19.5L38.25 4.34455V34.6554L0 19.5Z" fill="#00a000" />
+      <rect x="35" y="15" width="180" height="10" fill={brand500} />
+      <circle cx="230" cy="20" r="20" fill={brand500} />
+      <path d="M0 19.5L38.25 4.34455V34.6554L0 19.5Z" fill={brand500} />
       <foreignObject x="210" y="0" width="40" height="40">
         <Box
           aria-hidden
--- src/components/ArrowTwo.tsx
+++ src/components/ArrowTwo.tsx
@@ -1,6 +1,7 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, useToken } from "@chakra-ui/react";
 
 const ArrowTwo = () => {
+  const [brand500] = useToken("colors", ["brand2.500"]);
   return (
     <svg
       width="244"
@@ -9,10 +10,10 @@ const ArrowTwo = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect x="25" y="25" width="180" height="10" fill="#00a000" />
-      <rect x="25" y="25" width="10" height="120" fill="#00a000" />
-      <circle cx="202" cy="30" r="20" fill="#00a000" />
-      <path d="M30 170 L14.84455 133.75 H45.1554 L30 170Z" fill="#00a000" />
+      <rect x="25" y="25" width="180" height="10" fill={brand500} />
+      <rect x="25" y="25" width="10" height="120" fill={brand500} />
+      <circle cx="202" cy="30" r="20" fill={brand500} />
+      <path d="M30 170 L14.84455 133.75 H45.1554 L30 170Z" fill={brand500} />
       <foreignObject x="182" y="10" width="40" height="40">
         <Box
           aria-hidden
